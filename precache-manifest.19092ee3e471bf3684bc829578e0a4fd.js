d01ba1211aa844aed67e87366ffdf6f031ce7f53
pjh0426 <pjh0426@gmail.com>
2020-11-02 23:33:14 -0500

    Updates

---
+++ precache-manifest.19092ee3e471bf3684bc829578e0a4fd.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "807cc98d340d39be17c0abb719eb1dba",
+    "url": "/moah.github.io/moah/index.html"
+  },
+  {
+    "revision": "c166057544780f15ac39",
+    "url": "/moah.github.io/moah/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "401e5378f514f0134049",
+    "url": "/moah.github.io/moah/static/css/main.757a1f43.chunk.css"
+  },
+  {
+    "revision": "c166057544780f15ac39",
+    "url": "/moah.github.io/moah/static/js/2.0ac162f2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/moah.github.io/moah/static/js/2.0ac162f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "401e5378f514f0134049",
+    "url": "/moah.github.io/moah/static/js/main.b8dcc5b8.chunk.js"
+  },
+  {
+    "revision": "68ff4a194d0d51f9cdae",
+    "url": "/moah.github.io/moah/static/js/runtime-main.1d75d14b.js"
+  },
+  {
+    "revision": "01fd42b80a04a6020d0fdbd660900683",
+    "url": "/moah.github.io/moah/static/media/Cambodia_Inclass.01fd42b8.JPG"
+  },
+  {
+    "revision": "cd842f14ccdb758392b48959fc2b68b6",
+    "url": "/moah.github.io/moah/static/media/Event1.cd842f14.JPG"
+  },
+  {
+    "revision": "d1ae29ea527db6ada1946b0c07502b6c",
+    "url": "/moah.github.io/moah/static/media/Event2.d1ae29ea.jpg"
+  },
+  {
+    "revision": "fbc12e021a00859211a0426dc740171b",
+    "url": "/moah.github.io/moah/static/media/Event3.fbc12e02.jpg"
+  },
+  {
+    "revision": "37b27188ae593f2a27fc17045bfcef1c",
+    "url": "/moah.github.io/moah/static/media/Moah_logo_web_bg.37b27188.jpg"
+  },
+  {
+    "revision": "5f9667e21a78043e91538c3778fc726d",
+    "url": "/moah.github.io/moah/static/media/main.5f9667e2.jpg"
+  }
+]);
